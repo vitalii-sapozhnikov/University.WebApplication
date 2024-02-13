fa7de2adab679c829d229e8f85c14c247fab6539
Vitalii Sapozhnikov <vitalii.sapozhnikov@gmail.com>
2024-02-13 16:23:15 +0200

    DisciplinesAdded

---
+++ Models/Models/Department.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using University.WebApi.Models;
+
+namespace Models.Models
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+        public string Name { get; set; }
+
+        // One to mane Lecturers
+        public ICollection<Lecturer> Lecturers { get; set; }
+
+        public int HeadOfDepartmentId { get; set; }
+        public HeadOfDepartment HeadOfDepartment { get; set; }
+
+    }
+}
---
+++ Models/Models/HeadOfDepartment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Models
+{
+    public class HeadOfDepartment
+    {
+        public int HeadOfDepartmentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+
+        public ApplicationUser ApplicationUser { get; set; }
+
+        public int DepartmentId { get; set; }
+        public Department Department { get; set; }
+    }
+}
